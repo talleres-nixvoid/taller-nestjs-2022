4667b40a9d5ca6c60c59c797123f47fc1e41c16b
Misael Abanto <josemisaelabanto@gmail.com>
2022-09-01 18:35:44 -0500

    feat: config and typeorm modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { SaludoModule } from './saludo/saludo.module';
 
 @Module({
-  imports: [SaludoModule],
+  imports: [
+    ConfigModule.forRoot(),
+    SaludoModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, Logger],
 })
